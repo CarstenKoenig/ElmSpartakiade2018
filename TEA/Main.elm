710f59b5dd70a16e6b9d056befc5fde7fb647f1d
Carsten König <carsten@gettingsharper.de>
2018-03-14 10:11:46 +0100

    Lösung Übung

--- TEA/Main.elm
+++ TEA/Main.elm
@@ -18,7 +18,7 @@ type alias Model =
 
 
 type Msg
-    = Hochzählen
+    = Zählen Int
 
 
 view : Model -> Html Msg
@@ -26,12 +26,13 @@ view model =
     H.div
         []
         [ H.h3 [] [ H.text (toString model.zähler) ]
-        , H.button [ Ev.onClick Hochzählen ] [ H.text "+1" ]
+        , H.button [ Ev.onClick (Zählen 1) ] [ H.text "+1" ]
+        , H.button [ Ev.onClick (Zählen -1) ] [ H.text "-1" ]
         ]
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Hochzählen ->
-            { zähler = model.zähler + 1 }
+        Zählen inkrement ->
+            { zähler = model.zähler + inkrement }
