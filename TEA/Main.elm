5047d749d7e17de6d275d45ac7d3fce384b7b6ac
Carsten König <carsten@gettingsharper.de>
2018-03-14 10:20:57 +0100

    variables Inkrement hinzugefügt

--- TEA/Main.elm
+++ TEA/Main.elm
@@ -1,24 +1,29 @@
 module Main exposing (..)
 
 import Html as H exposing (Html)
+import Html.Attributes as Attr
 import Html.Events as Ev
 
 
 main : Program Never Model Msg
 main =
     H.beginnerProgram
-        { model = { zähler = 0 }
+        { model = { zähler = 0, inkrementText = "5" }
         , view = view
         , update = update
         }
 
 
 type alias Model =
-    { zähler : Int }
+    { zähler : Int
+    , inkrementText : String
+    }
 
 
 type Msg
     = Zählen Int
+    | InkrementTextSetzen String
+    | InkrementVerwenden
 
 
 view : Model -> Html Msg
@@ -28,6 +33,8 @@ view model =
         [ H.h3 [] [ H.text (toString model.zähler) ]
         , H.button [ Ev.onClick (Zählen 1) ] [ H.text "+1" ]
         , H.button [ Ev.onClick (Zählen -1) ] [ H.text "-1" ]
+        , H.input [ Attr.type_ "number", Attr.value model.inkrementText, Ev.onInput InkrementTextSetzen ] []
+        , H.button [ Ev.onClick InkrementVerwenden ] [ H.text "do" ]
         ]
 
 
@@ -35,4 +42,16 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         Zählen inkrement ->
-            { zähler = model.zähler + inkrement }
+            { model | zähler = model.zähler + inkrement }
+
+        InkrementTextSetzen text ->
+            { model | inkrementText = text }
+
+        InkrementVerwenden ->
+            String.toInt model.inkrementText
+                |> Result.map
+                    (\inkrement ->
+                        { model | zähler = model.zähler + inkrement }
+                    )
+                |> Result.withDefault
+                    { model | inkrementText = "5" }
